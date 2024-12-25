cd3d81b78f5f72212514717110d6e519e12bddb8
Seth Martineau <martineausw@gmail.com>
2024-12-25 14:22:42 -0500

    adjust string replacement from "service" to "peepee"

--- Source/content_script.js
+++ Source/content_script.js
@@ -5,9 +5,9 @@ function walk(node)
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
 	
-	var child, next;
+	let child, next;
 	
-	var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+	let tagName = node.tagName ? node.tagName.toLowerCase() : "";
 	if (tagName == 'input' || tagName == 'textarea') {
 		return;
 	}
@@ -39,10 +39,11 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\b(software-as-a-service|saas)\b/gi, "big peepee");
+	v = v.replace(/\b(platform-as-a-service|paas)\b/gi, "many big peepee")
+	v = v.replace(/\b(container-as-a-service|caas)\b/gi, "big peepee (wrapped)")
+	v = v.replace(/\b(infrastructure-as-a-service|iaas)\b/gi, "big peepee army");
+	v = v.replace(/\b(micro(?:-?))?service(s?)\b/gi, "$1peepee$2");
 	
 	textNode.nodeValue = v;
 }
